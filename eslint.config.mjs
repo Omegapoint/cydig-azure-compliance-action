1abcd83563345af393b31e588880373e07fb9ed8
Linnéa Oxenwaldt <linnea.oxenwaldt@omegapoint.se>
2026-01-26 10:47:31 +0100

    run format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,25 +5,24 @@ import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  {ignores: ['node_modules', 'test', 'dist', 'func']},
-  {
-    rules: {
-        '@typescript-eslint/explicit-function-return-type': 'warn',
-        '@typescript-eslint/no-inferrable-types': 0,
-        '@typescript-eslint/naming-convention': ['error', { selector: 'variableLike', format: ['camelCase'] }],
-        '@typescript-eslint/typedef': [
-            'error',
-            {
-                arrayDestructuring: true,
-                memberVariableDeclaration: true,
-                parameter: true,
-                propertyDeclaration: true,
-                variableDeclarationIgnoreFunction: true,
-            },
-        ],
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    { ignores: ['node_modules', 'test', 'dist', 'func'] },
+    {
+        rules: {
+            '@typescript-eslint/explicit-function-return-type': 'warn',
+            '@typescript-eslint/no-inferrable-types': 0,
+            '@typescript-eslint/naming-convention': ['error', { selector: 'variableLike', format: ['camelCase'] }],
+            '@typescript-eslint/typedef': [
+                'error',
+                {
+                    arrayDestructuring: true,
+                    memberVariableDeclaration: true,
+                    parameter: true,
+                    propertyDeclaration: true,
+                    variableDeclarationIgnoreFunction: true,
+                },
+            ],
+        },
     },
-}
-]
-);
+]);
